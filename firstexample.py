189ecb02ca935ec3662bb456d9ba857e2972fdba
housring <hoursing.com@gmail.com>
2023-10-01 13:10:45 +0700

    Print information on request

--- firstexample.py
+++ firstexample.py
@@ -3,4 +3,15 @@ import requests
 url = 'http://www.webscrapingfordatascience.com/basichttp/'
 request = requests.get(url)
 
+print("#Status code")
+print(request.status_code)
+print('#reason')
+print(request.reason)
+print('#headers')
+print(request.headers)
+print('#request')
+print(request.request)
+print('#request.headers')
+print(request.request.headers)
+print('#request.text')
 print(request.text)
