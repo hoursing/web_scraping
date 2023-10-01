2c75a049ccac3d4980b06496cde3301676fded22
housring <hoursing.com@gmail.com>
2023-10-01 14:12:45 +0700

    Scraping wiki

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# web_scraping
+# Overview
 Web scraping use python
+
+# Libraries
+ - Requests : pip install -U requests
+ - BeautifulSoup: pip install -U beautifulsoup4
+
+# Tools:
+    - VSC
+    - Python, Pylance, Code Spell Checker 
--- wikipedia_example.py
+++ wikipedia_example.py
@@ -1,4 +1,5 @@
 import requests
+from bs4 import BeautifulSoup
 
 url = 'https://en.wikipedia.org/w/index.php' + \
     '?title=List_of_Game_of_Thrones_episodes&oldid=802553687'
@@ -6,4 +7,36 @@ url = 'https://en.wikipedia.org/w/index.php' + \
 response = requests.get(url)
 html_contents = response.text
 
-print(response.text)
+html_soup = BeautifulSoup(html_contents, 'html.parser')
+
+# print(html_soup.find('h1'))
+# print(html_soup.find('', {'id': 'p-logo'}))
+# for found in html_soup.find_all(['h1', 'h2']):
+#     print(found)
+# print(html_soup)
+
+# We'll use a list to store our episode list
+episodes = []
+ep_tables = html_soup.find_all('table', class_='wikiepisodetable')
+print(ep_tables)
+
+for table in ep_tables:
+    headers = []
+    rows = table.find_all('tr')
+
+    # Start by fetching the header cells from the first row to determine
+    # The field names
+    for header in table.find('tr').find_all('th'):
+        headers.append(header.text)
+    # Then go though all the rows except the first one
+    for row in table.find('tr'):
+        values = []
+        # And get the column cells, the first one being inside a th-tag
+        for col in row.find_all(['th','td']):
+            values.append(col.text)
+        if values:
+            episode_dict = {headers[i]: values[i] for i in range(len(values))}
+            episodes.append(episode_dict)
+# Show result
+for episode in episodes:
+    print(episode)
